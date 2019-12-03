9f4fe34b64941317a2365f6b9c3376294e528d9e
NNK <sparkbyexamples@gmail.com>
2019-12-02 19:48:33 -0800

    RDD Example

--- src/main/scala/com/sparkbyexamples/spark/rdd/functions/aggregateExample.scala
+++ src/main/scala/com/sparkbyexamples/spark/rdd/functions/aggregateExample.scala
@@ -2,30 +2,26 @@ package com.sparkbyexamples.spark.rdd.functions
 
 import org.apache.spark.sql.SparkSession
 
-object aggregateExample {
+object aggregateExample extends App {
 
   val spark = SparkSession.builder()
-    .appName("SparkByExample")
+    .appName("SparkByExamples.com")
     .master("local")
     .getOrCreate()
 
   spark.sparkContext.setLogLevel("ERROR")
 
-  val rdd = spark.sparkContext.parallelize(
-    List("Germany","India","USA","USA","London","Russia","Mexico","Brazil","Canada","China")
-  )
-
-  val listRdd = spark.sparkContext.parallelize(List(9,2,3,4,5,6,7,8))
-
-  def param1= (accu:Int, v:Int) => accu + v
-  def param2= (accu1:Int,accu2:Int) => accu1 + accu2
-  println(listRdd.aggregate(3)(param1,param2))
+  //aggregate example
+  val listRdd = spark.sparkContext.parallelize(List(1,2,3,4,5,3,2))
+  def param0= (accu:Int, v:Int) => accu + v
+  def param1= (accu1:Int,accu2:Int) => accu1 + accu2
+  println("output 1 : "+listRdd.aggregate(0)(param0,param1))
 
 
-  val inputRDD = spark.sparkContext.parallelize(List(("A", 20),("B", 30),("C", 40)))
-
-  //aggregate
+  val inputRDD = spark.sparkContext.parallelize(List(("Z", 1),("A", 20),("B", 30),("C", 40),("B", 30),("B", 60)))
   def param3= (accu:Int, v:(String,Int)) => accu + v._2
   def param4= (accu1:Int,accu2:Int) => accu1 + accu2
-  println(inputRDD.aggregate(0)(param3,param4))
+  println("output 2 : "+inputRDD.aggregate(0)(param3,param4))
+
+
 }
