bb198e7756bfa047d1c011c0d9d354c787cee08d
NNK <sparkbyexamples@gmail.com>
2019-12-19 21:01:13 -0800

    Spark read & write text files

--- src/main/scala/com/sparkbyexamples/spark/dataframe/FromTextFile.scala
+++ src/main/scala/com/sparkbyexamples/spark/dataframe/FromTextFile.scala
@@ -1,6 +1,6 @@
 package com.sparkbyexamples.spark.dataframe
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
 object FromTextFile {
 
@@ -12,13 +12,33 @@ object FromTextFile {
       .getOrCreate()
 
     //returns DataFrame
-    val df = spark.read.text("src/main/resources/txt")
+    val df:DataFrame = spark.read.text("src/main/resources/csv/text01.txt")
     df.printSchema()
     df.show(false)
 
+    //converting to columns by splitting
+    import spark.implicits._
+    val df2 = df.map(f=>{
+      val elements = f.getString(0).split(",")
+      (elements(0),elements(1))
+    })
+
+    df2.printSchema()
+    df2.show(false)
+
     // returns Dataset[String]
-    val ds = spark.read.textFile("src/main/resources/txt")
+    val ds:Dataset[String] = spark.read.textFile("src/main/resources/csv/text01.txt")
     ds.printSchema()
     ds.show(false)
+
+    //converting to columns by splitting
+    import spark.implicits._
+    val ds2 = ds.map(f=> {
+     val elements = f.split(",")
+      (elements(0),elements(1))
+    })
+
+    ds2.printSchema()
+    ds2.show(false)
   }
 }
