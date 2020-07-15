1acd8f48d783f9ccd1f80897d2ce81a714935c35
NNK <sparkbyexamples@gmail.com>
2020-07-14 21:00:03 -0700

    Sppary Array example

--- src/main/scala/com/sparkbyexamples/spark/dataframe/functions/collection/ArrayOfString.scala
+++ src/main/scala/com/sparkbyexamples/spark/dataframe/functions/collection/ArrayOfString.scala
@@ -42,4 +42,9 @@ object ArrayOfString extends App{
 
   df3.toDF("Name","Languages","currentState")
      .show(false)
+
+  df.createOrReplaceTempView("ARRAY_STRING")
+  spark.sql("select name, concat_ws(',',languagesAtSchool) as languagesAtSchool," +
+    " currentState from ARRAY_STRING")
+    .show(false)
 }
