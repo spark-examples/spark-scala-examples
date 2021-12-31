89dc05f7320bdea3dc9aa5c32cbb637be90c9c3b
loukritaki <56266938+loukritia19@users.noreply.github.com>
2021-12-31 02:41:27 +0200

    error-kmeans

--- src/main/scala/com/sparkbyexamples/spark/SparkSessionTest.scala
+++ src/main/scala/com/sparkbyexamples/spark/SparkSessionTest.scala
@@ -1,20 +1,12 @@
-//package org.apache.spark.examples.mllib
+package org.apache.spark.examples.mllib
 
 import org.apache.spark.{SparkConf, SparkContext}
 // $example on$
-import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
+import org.apache.spark.mllib.clustering.{KMeans}
 import org.apache.spark.mllib.linalg.Vectors
 
 object kmeansExample {
   def main(args: Array[String]): Unit = {
-
-    println("***********************************************************************************************")
-    println("***********************************************************************************************")
-
-    println("Hello, Spark!")
-
-    println("***********************************************************************************************")
-    println("***********************************************************************************************")
 
     val conf = new SparkConf()
       .setMaster("local[2]")
@@ -23,7 +15,7 @@ object kmeansExample {
 
     // $example on$
     // Load and parse the data
-    val data = sc.textFile("part10K.txt")
+    val data = sc.textFile("src/main/resources/newData.txt")
     val parsedData = data.map(s => Vectors.dense(s.split(' ').map(_.toDouble))).cache()
 
 
